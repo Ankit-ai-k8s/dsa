35c6563d4a6a82f898e0060958229a4db93340c8
Ankit Singh <ankit.centilytics@gmail.com>
2026-06-28 23:07:13 +0530

    Time: 1 ms (66.25%), Space: 3.9 MB (26.25%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.go
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.go
@@ -1,17 +1,5 @@
 func defangIPaddr(ad string) string {
-	chs := []byte(ad)
-	res := ""
-	n := len(ad)
-
-	for i := 0; i < n; i++ {
-		ch := chs[i]
-
-		if ch == '.' {
-			res = res + "[.]"
-		} else {
-			res = res + string(ch)
-		}
-	}
-    
-	return res
+    chs :=  strings.Split(ad, ".")
+    res := strings.Join(chs, "[.]")
+    return res
 }
