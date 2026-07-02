10105e28f8ee24c3bdd108b66ba258ea930dc25a
Ankit Singh <ankit.centilytics@gmail.com>
2026-07-03 01:00:22 +0530

    Time: 26 ms (46.62%), Space: 8 MB (51.01%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
@@ -1,21 +1,20 @@
 func firstUniqChar(s string) int {
-    m := map[string][2]int{}
-    mn := math.MaxInt
-    for i,ch := range strings.Split(s, "") {
-        v := m[ch]
-        v[0]++
-        v[1] = i
-        m[ch] = v
-    }
-    fmt.Println(m)
-    for _,v := range m {
-
-        if v[0] == 1 && mn > v[1]{
-            mn = v[1]
-        }
-    }
-    if mn == math.MaxInt {
-        return -1
-    }
-    return mn
+	m := map[rune][2]int{}
+	mn := math.MaxInt
+	for i, ch := range s {
+		v := m[ch]
+		v[0]++
+		v[1] = i
+		m[ch] = v
+	}
+	// fmt.Println(m)
+	for _, v := range m {
+		if v[0] == 1 && mn > v[1] {
+			mn = v[1]
+		}
+	}
+	if mn == math.MaxInt {
+		return -1
+	}
+	return mn
 }
