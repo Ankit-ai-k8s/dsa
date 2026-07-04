3cf448c6a7a2f5bd70d6764e928b3b9804bbb6e0
Ankit Singh <ankit.centilytics@gmail.com>
2026-07-05 01:38:16 +0530

    Time: 2 ms (6.23%), Space: 7.3 MB (5.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.go
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -1,11 +1,12 @@
 func longestCommonPrefix(strs []string) string {
-	slices.SortFunc(strs, func(a, b string) int { return len(a) - len(b) })
+	// slices.SortFunc(strs, func(a, b string) int { return len(a) - len(b) })
 	res := strs[0]
 	for i := 1; i < len(strs); i++ {
 		s2 := strs[i]
 		cur := ""
 		n := len(res)
-		for j := 0; j < n; j++ {
+        m := len(s2)
+		for j := 0; j < n && j < m; j++ {
 			if res[j] == s2[j] {
 				cur = cur + string(res[j])
 			}else {
