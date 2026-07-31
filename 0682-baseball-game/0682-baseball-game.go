7b00165ca21171a461b9e6ab7b70537f91e4dc5b
Ankit Singh <ankit.centilytics@gmail.com>
2026-08-01 02:46:25 +0530

    Time: 0 ms (100%), Space: 4.6 MB (10.11%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.go
@@ -0,0 +1,48 @@
+func calPoints(operations []string) int {
+	st := []int{}
+
+	for _,op := range operations {
+
+		nm, err := strconv.Atoi(op)
+
+		if err == nil {
+			push(&st, nm)
+		} else {
+			if op == "C" {
+				pop(&st)
+			} else if op == "+" {
+				n2,_ := pop(&st)
+				n1,_ := pop(&st)
+				sm := n1 + n2
+				push(&st, n1)
+				push(&st, n2)
+				push(&st, sm)
+			} else {
+				st = append(st, st[len(st)-1]*2)
+			}
+		}
+	}
+
+    // fmt.Println(st)
+
+    res:=0
+    for _,nm := range st {
+        res += nm
+    }
+
+    return res
+}
+
+func push(st *[]int, ele int) {
+	*st = append(*st, ele)
+}
+
+func pop(st *[]int) (int, bool) {
+	if len(*st) == 0 {
+		return -1, false
+	}
+	last := len(*st) - 1
+	ele := (*st)[last]
+	*st = (*st)[:last]
+	return ele, true
+}
